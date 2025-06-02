cad80d3a3a906a6ac216be58a73147a58af58aa6
Kerkel <134325525+drpandacat@users.noreply.github.com>
2025-06-02 18:01:49 -0400

    Update compathelper.lua

--- compathelper.lua
+++ compathelper.lua
@@ -1,58 +1,58 @@
-local VERSION = 1.1
+local VERSION = 1
 
 ---@type table<string, function>
 local entries = {}
 
-if CompatHelper then
-    if CompatHelper.VERSION > VERSION then
+if ModCompatHelper then
+    if ModCompatHelper.VERSION > VERSION then
         return
     end
 
-    entries = CompatHelper.Entries
+    entries = ModCompatHelper.Entries
 
     if REPENTOGON then
-        CompatHelper:RemoveCallback(ModCallbacks.MC_POST_MODS_LOADED, CompatHelper.Load)
+        ModCompatHelper:RemoveCallback(ModCallbacks.MC_POST_MODS_LOADED, ModCompatHelper.Load)
     end
 
-    CompatHelper:RemoveCallback(ModCallbacks.MC_POST_GAME_STARTED, CompatHelper.Load)
+    ModCompatHelper:RemoveCallback(ModCallbacks.MC_POST_GAME_STARTED, ModCompatHelper.Load)
 end
 
-CompatHelper = RegisterMod("Mod Compatibility Helper", 1)
-CompatHelper.VERSION = VERSION
-CompatHelper.Entries = entries
+ModCompatHelper = RegisterMod("Mod Compatibility Helper", 1)
+ModCompatHelper.VERSION = VERSION
+ModCompatHelper.Entries = entries
 
-function CompatHelper:Load()
-    for _, v in ipairs(CompatHelper.Entries) do
+function ModCompatHelper:Load()
+    for _, v in ipairs(ModCompatHelper.Entries) do
         if _G[v[1]] then
             v[2]()
         end
     end
 end
 
-function CompatHelper:Register(mod, global, fn)
-    CompatHelper.Entries[#CompatHelper.Entries + 1] = {global, fn, mod}
+function ModCompatHelper:Register(mod, global, fn)
+    ModCompatHelper.Entries[#ModCompatHelper.Entries + 1] = {global, fn, mod}
 end
 
-function CompatHelper:Init()
+function ModCompatHelper:Init()
     if Isaac.GetFrameCount() > 0 then
-        CompatHelper:Load()
+        ModCompatHelper:Load()
     end
 end
 
-function CompatHelper:Clear(mod)
+function ModCompatHelper:Clear(mod)
     local copy = {}
 
-    for _, v in ipairs(CompatHelper.Entries) do
+    for _, v in ipairs(ModCompatHelper.Entries) do
         if v[3].Name ~= mod.Name then
             copy[#copy + 1] = v
         end
     end
 
-    CompatHelper.Entries = copy
+    ModCompatHelper.Entries = copy
 end
 
 if REPENTOGON then
-    CompatHelper:AddCallback(ModCallbacks.MC_POST_MODS_LOADED, CompatHelper.Load)
+    ModCompatHelper:AddCallback(ModCallbacks.MC_POST_MODS_LOADED, ModCompatHelper.Load)
 else
-    CompatHelper:AddCallback(ModCallbacks.MC_POST_GAME_STARTED, CompatHelper.Load)
+    ModCompatHelper:AddCallback(ModCallbacks.MC_POST_GAME_STARTED, ModCompatHelper.Load)
 end
